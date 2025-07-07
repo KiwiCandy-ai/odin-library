bb357bbd2655275b8d9b0af846b7aa7c095a14b7
KiwiCandy-ai <206121829+KiwiCandy-ai@users.noreply.github.com>
2025-07-07 21:13:01 +0100

    Delete buton functionality

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
-const myLibrary = []
+const myLibrary = [ 
+    {title: 'The Hobbit',
+    author: 'JRR Tolkien', 
+    id: crypto.randomUUID(),
+    pages: '256 pages',
+    read: 'read'
+    },
+    {title: 'War & Peace',
+    author: 'Leo Tolstoy',
+    id: crypto.randomUUID(),
+    pages: '900 pages',
+    read: 'read'
+    }
+]
 const dialog = document.querySelector('dialog')
 const newButton = document.querySelector('dialog + button')
 const addButton = document.querySelector('dialog button')
-document.body.appendChild(newButton)
 const display = document.createElement('div')
 display.classList.add('display')
 document.body.appendChild(display)
@@ -19,6 +31,7 @@ function Book(title, author, id, pages, read) {
 }
 
 
+
 function addBookToLibrary(title, author, id, pages, read) {
     title = document.getElementById('title').value
     author = document.getElementById('author').value
@@ -32,8 +45,6 @@ function addBookToLibrary(title, author, id, pages, read) {
     read.value = ''
   
 }
-
-
 
 newButton.addEventListener('click', () => {
     dialog.showModal()
@@ -52,22 +63,49 @@ addButton.addEventListener('click', () => {
 
 function addToTable() {
     const table = document.getElementById('table')
+
     let child = table.lastElementChild
     while (child) {
         table.removeChild(child)
         child = table.lastElementChild
     }
+    const headerRow = document.createElement('tr');
+
+            Object.keys(myLibrary[0]).forEach(key => {
+                const th = document.createElement('th');
+                th.appendChild(document.createTextNode(key));
+                headerRow.appendChild(th);
+            });
+            table.appendChild(headerRow);
     myLibrary.forEach(item => {
         const row = document.createElement('tr')
+            row.classList.add('row')
         Object.values(item).forEach(value => {
         const td = document.createElement('td')
         td.classList.add('data')
         td.appendChild(document.createTextNode(value))
         row.appendChild(td)
+        
         })
         table.appendChild(row)
+        const remove = document.createElement('button')
+        remove.classList.add('remove')
+        row.appendChild(remove)
+        remove.addEventListener('click', removeTasks)
+        const checkbox = document.createElement('input')
+        checkbox.type = 'checkbox'
+        row.appendChild(checkbox)
+    
     })
+    table.appendChild(newButton)
     display.appendChild(table)
 }
 
+const remove = document.querySelectorAll('.remove')
+addToTable()
+
+function removeTasks(e) {
+    const parentElement = this.parentElement
+    parentElement.remove()
+}
 

75dd8f4dee45d3abb011690cdec2bf489aafee91
KiwiCandy-ai <206121829+KiwiCandy-ai@users.noreply.github.com>
2025-07-12 20:54:04 +0100

    Fix checkbox/button positions

--- main.js
+++ main.js
@@ -3,34 +3,45 @@ const myLibrary = [
     author: 'JRR Tolkien', 
     id: crypto.randomUUID(),
     pages: '256 pages',
-    read: 'read'
+    read: 'not read'
     },
     {title: 'War & Peace',
     author: 'Leo Tolstoy',
     id: crypto.randomUUID(),
     pages: '900 pages',
-    read: 'read'
+    read: 'not read'
     }
 ]
 const dialog = document.querySelector('dialog')
 const newButton = document.querySelector('dialog + button')
 const addButton = document.querySelector('dialog button')
 const display = document.createElement('div')
+const nav = document.getElementById('nav')
 display.classList.add('display')
 document.body.appendChild(display)
 const list = document.createElement('ul')
 // list.classList.add('list')
 display.appendChild(list)
+const heading = document.createElement('h1')
+heading.textContent = 'Book Library'
+heading.classList.add('heading')
+nav.appendChild(heading)
+nav.appendChild(newButton)
+
+const checkbox = document.getElementById('read')
 
 function Book(title, author, id, pages, read) {
     this.title = title;
     this.author = author;
     this.id = crypto.randomUUID();
     this.pages = pages;
-    this.read = read;
+    this.read = {}
+    if (checkbox.checked) {
+        this.read = 'read' 
+    } else {
+        this.read = 'not read'
+    }
 }
-
-
 
 function addBookToLibrary(title, author, id, pages, read) {
     title = document.getElementById('title').value
@@ -77,7 +88,7 @@ function addToTable() {
                 headerRow.appendChild(th);
             });
             table.appendChild(headerRow);
-    myLibrary.forEach(item => {
+        myLibrary.forEach(item => {
         const row = document.createElement('tr')
             row.classList.add('row')
         Object.values(item).forEach(value => {
@@ -85,19 +96,25 @@ function addToTable() {
         td.classList.add('data')
         td.appendChild(document.createTextNode(value))
         row.appendChild(td)
-        
         })
         table.appendChild(row)
+        td = document.createElement('td')
+        td.classList.add('data')
+        const status = document.createElement('input')
+        status.type = 'checkbox'
+        status.classList.add('checkbox')
+        // status.addEventListener('change', toggleStatus)
+        td.appendChild(status)
+        row.appendChild(td)
+        td = document.createElement('td')
+        td.classList.add('data')
         const remove = document.createElement('button')
-        remove.classList.add('remove')
-        row.appendChild(remove)
         remove.addEventListener('click', removeTasks)
-        const checkbox = document.createElement('input')
-        checkbox.type = 'checkbox'
-        row.appendChild(checkbox)
+        remove.classList.add('remove')
+        td.appendChild(remove)
+        row.appendChild(td)
     
     })
-    table.appendChild(newButton)
     display.appendChild(table)
 }
 
@@ -108,4 +125,8 @@ function removeTasks(e) {
     const parentElement = this.parentElement
     parentElement.remove()
 }
+
+
+
+
 
